ebf6411dd94a4354222766b5193550884c0a3f1a
shinjunseok <sjsage522@gmail.com>
2021-04-19 17:12:40 +0900

    Initial commit

---
+++ src/main/java/io/wisoft/vamos/VamosApplication.java
@@ -0,0 +1,13 @@
+package io.wisoft.vamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VamosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/wisoft/vamos/VamosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wisoft.vamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
