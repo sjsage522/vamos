4269f879b84469859b23be5e5f9e971db0c15feb
shinjunseok <sjsage522@gmail.com>
2021-04-29 18:50:32 +0900

    add : Audit 기능
    BaseTimeEntity 추가.

--- src/main/java/io/wisoft/vamos/VamosApplication.java
+++ src/main/java/io/wisoft/vamos/VamosApplication.java
@@ -2,8 +2,10 @@ package io.wisoft.vamos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VamosApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/io/wisoft/vamos/domain/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package io.wisoft.vamos.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+}
