22acc74ae4b47dee1275488e8edd06edddf5bd25
shinjunseok <sjsage522@gmail.com>
2021-05-18 08:57:18 +0900

    add : custom exception
    데이터 중복 예외처리 구현

---
+++ src/main/java/io/wisoft/vamos/exception/DataAlreadyExistsException.java
@@ -0,0 +1,24 @@
+package io.wisoft.vamos.exception;
+
+public class DataAlreadyExistsException extends RuntimeException {
+
+    public DataAlreadyExistsException() {
+        super();
+    }
+
+    public DataAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public DataAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+
+    protected DataAlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
