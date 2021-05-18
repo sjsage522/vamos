06a5675a329349bb5338997086f1f44c50da9424
shinjunseok <sjsage522@gmail.com>
2021-05-18 09:01:36 +0900

    add : rest controller advice
    예외 처리 핸들러 구현

---
+++ src/main/java/io/wisoft/vamos/controller/api/handler/RestControllersExceptionHandler.java
@@ -0,0 +1,38 @@
+package io.wisoft.vamos.controller.api.handler;
+
+import io.wisoft.vamos.controller.api.ApiResult;
+import io.wisoft.vamos.exception.DataAlreadyExistsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class RestControllersExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    protected ApiResult<Object> methodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+        return ApiResult.failed(ex
+                .getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody
+    protected ApiResult<Object> illegalArgumentException(
+            IllegalArgumentException ex) {
+        return ApiResult.failed(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataAlreadyExistsException.class)
+    @ResponseBody
+    protected ApiResult<Object> dataAlreadyExistsException(
+            DataAlreadyExistsException ex) {
+        return ApiResult.failed(ex.getMessage());
+    }
+}
