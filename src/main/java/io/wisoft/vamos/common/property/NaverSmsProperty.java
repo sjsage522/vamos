e8af81dc66c4ba1b332a122ced2afb62591d5f26
shinjunseok <sjsage522@gmail.com>
2021-05-30 01:17:44 +0900

    add : naverSmsProperty
    설정정보에서 property 값들을 읽음

---
+++ src/main/java/io/wisoft/vamos/common/property/NaverSmsProperty.java
@@ -0,0 +1,18 @@
+package io.wisoft.vamos.common.property;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@RequiredArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties("ncs")
+public class NaverSmsProperty {
+
+    private final String accessKey;
+    private final String serviceId;
+    private final String secretKey;
+    private final String from;
+}
