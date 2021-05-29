4c10d5fea476e1c305e4adb5e433cca7a0c54e5c
shinjunseok <sjsage522@gmail.com>
2021-05-30 01:18:28 +0900

    add : property config
    설정정보에서 property 값들을 읽을 수 있도록 configuration 클래스 생성

---
+++ src/main/java/io/wisoft/vamos/common/config/PropertyConfiguration.java
@@ -0,0 +1,10 @@
+package io.wisoft.vamos.common.config;
+
+import io.wisoft.vamos.common.property.NaverSmsProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(value = {NaverSmsProperty.class})
+public class PropertyConfiguration {
+}
