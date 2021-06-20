3173ba565bf59cc5011b30675e61c8a2776d4acc
shinjunseok <sjsage522@gmail.com>
2021-06-20 16:29:30 +0900

    update : user service test
    사용자 회원가입 테스트 추가

--- src/test/java/io/wisoft/vamos/service/UserServiceTest.java
+++ src/test/java/io/wisoft/vamos/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import io.wisoft.vamos.domain.user.Authority;
 import io.wisoft.vamos.domain.user.PhoneNumber;
 import io.wisoft.vamos.domain.user.User;
 import io.wisoft.vamos.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,12 +13,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collections;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -28,11 +26,24 @@ class UserServiceTest {
     @Mock
     PasswordEncoder passwordEncoder;
 
+    UserService userService;
+
+    @BeforeEach
+    void serviceInit() {
+        userService = new UserService(userRepository, passwordEncoder);
+    }
+
+    @BeforeEach
+    void userCreate() {
+
+    }
+
     @Test
     @DisplayName("사용자 회원가입 성공 테스트")
     void _01_user_join_success_test() {
 
         //given
+
         PhoneNumber phoneNumber = PhoneNumber.of("01012345678");
         User user = User.from("testId", "1234", phoneNumber, "tester");
 
@@ -42,8 +53,6 @@ class UserServiceTest {
                 .willReturn(0);
 
         //when
-        UserService userService = new UserService(userRepository, passwordEncoder);
-
         userService.join(user);
 
         //then
@@ -51,18 +60,51 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("사용자 회원가입 실패 테스트")
+    @DisplayName("사용자 회원가입 실패 테스트 (사용자 닉네임 중복)")
     void _02_user_join_failed_test() {
 
         //given
-        PhoneNumber phoneNumber = PhoneNumber.of("01012345678");
+        PhoneNumber phoneNumber = PhoneNumber.of("01023456789");
+        User user = User.from("testId2", "1234", phoneNumber, "tester");
+
+        given(userRepository.findDuplicateUserCount("testId2", phoneNumber, "tester"))
+                .willReturn(1);
+
+        //when
+
+        //then
+        assertThrows(DataAlreadyExistsException.class, () -> userService.join(user));
+    }
+
+    @Test
+    @DisplayName("사용자 회원가입 실패 테스트 (사용자 아이디 중복)")
+    void _03_user_join_failed_test() {
+
+        //given
+        PhoneNumber phoneNumber = PhoneNumber.of("01023456789");
+        User user = User.from("testId", "1234", phoneNumber, "tester2");
+
+        given(userRepository.findDuplicateUserCount("testId", phoneNumber, "tester2"))
+                .willReturn(1);
+
+        //when
+
+        //then
+        assertThrows(DataAlreadyExistsException.class, () -> userService.join(user));
+    }
+
+    @Test
+    @DisplayName("사용자 회원가입 실패 테스트 (사용자 전화번호 중복)")
+    void _04_user_join_failed_test() {
+
+        //given
+        PhoneNumber phoneNumber = PhoneNumber.of("01023456789");
         User user = User.from("testId", "1234", phoneNumber, "tester");
 
         given(userRepository.findDuplicateUserCount("testId", phoneNumber, "tester"))
                 .willReturn(1);
 
         //when
-        UserService userService = new UserService(userRepository, passwordEncoder);
 
         //then
         assertThrows(DataAlreadyExistsException.class, () -> userService.join(user));
