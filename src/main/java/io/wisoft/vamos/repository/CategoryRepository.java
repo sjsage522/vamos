459b681aa97e1bb1843b839ab8adc0d773828fa5
shinjunseok <sjsage522@gmail.com>
2021-06-23 15:07:43 +0900

    update : category repo
    카테고리이름으로 카테고리 조회 메서드 추가

--- src/main/java/io/wisoft/vamos/repository/CategoryRepository.java
+++ src/main/java/io/wisoft/vamos/repository/CategoryRepository.java
@@ -1,7 +1,12 @@
 package io.wisoft.vamos.repository;
 
 import io.wisoft.vamos.domain.category.Category;
+import io.wisoft.vamos.domain.category.CategoryName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(CategoryName name);
 }
