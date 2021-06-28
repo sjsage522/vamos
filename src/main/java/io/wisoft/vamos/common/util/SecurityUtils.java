60e6562ab6588d98de7fc445014f81881e6cd5d1
shinjunseok <sjsage522@gmail.com>
2021-06-28 18:34:28 +0900

    update : SecurityUtils
    로직 간소화

--- src/main/java/io/wisoft/vamos/common/util/SecurityUtils.java
+++ src/main/java/io/wisoft/vamos/common/util/SecurityUtils.java
@@ -1,41 +1,17 @@
 package io.wisoft.vamos.common.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Optional;
 
 public class SecurityUtils {
-
-    private static final Logger logger = LoggerFactory.getLogger(SecurityUtils.class);
-
-    private SecurityUtils() {
-    }
 
     /**
      * security context 에서의 authenticate 객체를 이용하여 username 을 리턴해주는 메서드
      * @return
      */
-    public static Optional<String> getCurrentUsername() {
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null) {
-            logger.debug("Security Context 에 인증 정보가 없습니다.");
-            return Optional.empty();
-        }
-
-        String username = null;
-        if (authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
-            username = springSecurityUser.getUsername();
-        } else if (authentication.getPrincipal() instanceof String) {
-            username = (String) authentication.getPrincipal();
-        }
-
-        return Optional.ofNullable(username);
+    public static String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
     }
 }
 
