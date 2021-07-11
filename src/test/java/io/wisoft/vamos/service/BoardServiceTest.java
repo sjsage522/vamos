06e84436fd7effd105c6977f1fadbb7b1e0041d2
shinjunseok <sjsage522@gmail.com>
2021-07-11 15:32:56 +0900

    fix : board service test
    테스트 수정

--- src/test/java/io/wisoft/vamos/service/BoardServiceTest.java
+++ src/test/java/io/wisoft/vamos/service/BoardServiceTest.java
@@ -1,34 +1,41 @@
 package io.wisoft.vamos.service;
 
 import io.wisoft.vamos.domain.board.Board;
-import io.wisoft.vamos.domain.board.BoardStatus;
+import io.wisoft.vamos.domain.category.Category;
 import io.wisoft.vamos.domain.uploadphoto.UploadFile;
+import io.wisoft.vamos.domain.user.PhoneNumber;
 import io.wisoft.vamos.domain.user.User;
+import io.wisoft.vamos.domain.user.UserLocation;
+import io.wisoft.vamos.dto.board.BoardUploadRequest;
 import io.wisoft.vamos.repository.BoardRepository;
+import io.wisoft.vamos.repository.CategoryRepository;
 import io.wisoft.vamos.repository.UploadFileRepository;
+import io.wisoft.vamos.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
-import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
@@ -44,32 +51,75 @@ class BoardServiceTest {
     @Mock
     UploadFileRepository uploadFileRepository;
 
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @BeforeEach
+    public void setUp() {
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(
+                        new UsernamePasswordAuthenticationToken(
+                                "testUser",
+                                "password",
+                                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+                        )
+                );
+    }
+
     @Test
     @DisplayName("게시글 업로드 테스트 (첨부이미지가 없는 경우)")
     void board_upload_not_include_files_test() {
         //given
-        Board board = mock(Board.class);
+        BoardUploadRequest boardUploadRequest = getBoardUploadRequest("title", "content", 1000, "DIGITAL_DEVICE");
+
         UploadFile uploadFile = mock(UploadFile.class);
 
-        given(boardRepository.save(board))
+        String username = "testUser";
+        User user = getUser(username);
+
+        Category category = getCategory(boardUploadRequest);
+
+        Board board = getBoard(user, category, "title", "content", 1000);
+
+        given(userRepository.findByUsername(username))
+                .willReturn(java.util.Optional.of(user));
+        given(categoryRepository.findByName(category.getName()))
+                .willReturn(java.util.Optional.of(category));
+        given(boardRepository.save(any()))
                 .willReturn(board);
+
         List<UploadFile> uploadFiles = List.of(uploadFile);
 
         //when
-        boardService.upload(board, null);
+        boardService.upload(boardUploadRequest, null);
 
         //then
-        then(boardRepository).should(times(1)).save(board);
         then(uploadFileRepository).should(times(0)).saveAll(uploadFiles);
         assertThat(board.getUploadFiles().size()).isEqualTo(0);
+    }
+
+    private Board getBoard(User user, Category category, String title, String content, int price) {
+        return Board.from(title, content, price, user, category);
     }
 
     @Test
     @DisplayName("게시글 업로드 테스트 (첨부이미지가 있는 경우)")
     void board_upload_include_files_test() throws IOException {
         //given
-        Board board = mock(Board.class);
+        BoardUploadRequest boardUploadRequest = getBoardUploadRequest("title", "content", 1000, "DIGITAL_DEVICE");
+
+        String username = "testUser";
+        User user = getUser(username);
+
+        Category category = getCategory(boardUploadRequest);
+
+        Board board = getBoard(user, category, "title", "content", 1000);
         FileInputStream fis = new FileInputStream("/Users/jun/Downloads/penguin.png");
+
         MockMultipartFile multipartFile = new MockMultipartFile(
                 "files",
                 "penguin.png",
@@ -77,72 +127,40 @@ class BoardServiceTest {
                 fis);
         MultipartFile[] multipartFiles = {multipartFile};
 
-        given(boardRepository.save(board))
+        given(userRepository.findByUsername(username))
+                .willReturn(java.util.Optional.of(user));
+        given(boardRepository.save(any()))
                 .willReturn(board);
         given(uploadFileRepository.saveAll(any()))
                 .willReturn(any());
+        given(categoryRepository.findByName(category.getName()))
+                .willReturn(java.util.Optional.of(category));
 
         //when
-        boardService.upload(board, multipartFiles);
+        boardService.upload(boardUploadRequest, multipartFiles);
 
         //then
-        then(boardRepository).should(times(1)).save(board);
         then(uploadFileRepository).should(times(1)).saveAll(any());
     }
 
-    @Test
-    @DisplayName("게시글 수정 실패 테스트 (다른 사용자의 게시글은 수정할 수 없음)")
-    void board_update_failed_test() {
-        //given
-        User user = mock(User.class);
-        Board board = mock(Board.class);
-
-        given(boardRepository.findById(1L))
-                .willReturn(java.util.Optional.of(board));
-        given(board.getUser())
-                .willReturn(user);
-        setField(user, "username", "writer");
-        setField(user, "nickname", "writer22");
-        System.out.println(user);
-        System.out.println(user.getNickname());
-
-        User otherUser = mock(User.class);
-        Board otherBoard = mock(Board.class);
-
-        given(otherBoard.getUser())
-                .willReturn(otherUser);
-        setField(otherUser, "username", "writer");
-        setField(otherUser, "nickname", "writer22");
-
-
-        //then
-        assertThrows(IllegalStateException.class,
-                () -> boardService.update(1L, otherBoard));
+    private Category getCategory(BoardUploadRequest boardUploadRequest) {
+        return Category.of(boardUploadRequest.getCategoryNameEN());
     }
 
-    @Test
-    @DisplayName("게시글 수정 성공 테스트")
-    void board_update_success_test() {
-        //given
-        User user = mock(User.class);
-        setField(user, "username", "writer");
-        setField(user, "nickname", "writer22");
-        Board board = mock(Board.class, Mockito.RETURNS_DEEP_STUBS);
-        setField(board, "id", 1L);
-        setField(board, "user", user);
-        setField(board, "status", BoardStatus.SALE);
+    private User getUser(String username) {
+        return User.from(username,
+                "1234",
+                PhoneNumber.of("01012345678"),
+                "tester",
+                UserLocation.from(0.0, 0.0, "tset location"));
+    }
 
-        System.out.println(board.getId());
-
-        given(boardRepository.findById(1L))
-                .willReturn(java.util.Optional.of(board));
-
-
-        //when
-        board.changeStatus(BoardStatus.RESERVE);
-        Board update = boardService.update(1L, board);
-
-        //then
-        assertThat(update.getStatus()).isEqualTo(BoardStatus.RESERVE);
+    private BoardUploadRequest getBoardUploadRequest(String title, String content, int price, String categoryNameEN) {
+        BoardUploadRequest boardUploadRequest = new BoardUploadRequest();
+        boardUploadRequest.setTitle(title);
+        boardUploadRequest.setContent(content);
+        boardUploadRequest.setPrice(price);
+        boardUploadRequest.setCategoryNameEN(categoryNameEN);
+        return boardUploadRequest;
     }
 }
