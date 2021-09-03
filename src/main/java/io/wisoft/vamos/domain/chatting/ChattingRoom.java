a13b952fbd1304533b853f0e09791e79a162d02a
shinjunseok <sjsage522@gmail.com>
2021-09-03 19:50:56 +0900

    update : ChattingRoom.java
    - field 추가 및 수정

--- src/main/java/io/wisoft/vamos/domain/chatting/ChattingRoom.java
+++ src/main/java/io/wisoft/vamos/domain/chatting/ChattingRoom.java
@@ -3,9 +3,12 @@ package io.wisoft.vamos.domain.chatting;
 import io.wisoft.vamos.domain.BaseTimeEntity;
 import io.wisoft.vamos.domain.board.Board;
 import io.wisoft.vamos.domain.user.User;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "chatting_room")
@@ -26,14 +29,38 @@ public class ChattingRoom extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @JoinColumn(name = "seller_id")
+    private User seller;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id")
+    private User buyer;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
 
+    @OneToMany(mappedBy = "chattingRoom")
+    private List<ChattingContent> chattingContent = new ArrayList<>();
+
     @Column(name = "chatting_category")
     @Enumerated(EnumType.STRING)
     private ChattingCategory category;
+
+    @Builder
+    private ChattingRoom(User buyer, Board board, List<ChattingContent> chattingContent, ChattingCategory category) {
+        this.seller = board.getUser();
+        this.buyer = buyer;
+        this.board = board;
+        this.chattingContent = chattingContent;
+        this.category = category;
+    }
+
+    public void updateChatContent(List<ChattingContent> chattingContentList) {
+        chattingContentList.forEach(content -> content.registerChatRoom(this));
+    }
+
+    protected ChattingRoom() {
+
+    }
 }
