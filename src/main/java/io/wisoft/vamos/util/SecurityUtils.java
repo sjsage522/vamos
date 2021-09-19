87fbd62fc543e9d1e61b71f9a7776fe9c440c3cc
shinjunseok <sjsage522@gmail.com>
2021-09-19 22:17:45 +0900

    add : SecurityUtils.java
    - 시큐리티 유틸 기능 클래스 추가

---
+++ src/main/java/io/wisoft/vamos/util/SecurityUtils.java
@@ -0,0 +1,17 @@
+package io.wisoft.vamos.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtils {
+
+    public static Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getName() == null) {
+            throw new RuntimeException("Security Context 에 인증 정보가 없습니다.");
+        }
+
+        return Long.parseLong(authentication.getName());
+    }
+}
