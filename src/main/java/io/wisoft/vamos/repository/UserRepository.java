37c7cb146f151c32eae9130d5edb63002d9cff41
shinjunseok <sjsage522@gmail.com>
2021-09-28 17:24:30 +0900

    update : UserRepository.java
    - 사용자 이름으로 사용자 조회 메서드 추가

--- src/main/java/io/wisoft/vamos/repository/UserRepository.java
+++ src/main/java/io/wisoft/vamos/repository/UserRepository.java
@@ -9,5 +9,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
+    Optional<User> findByUsername(String username);
+
     Boolean existsByEmail(String email);
 }
