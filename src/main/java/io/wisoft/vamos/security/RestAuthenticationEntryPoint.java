59cb36c28407c80570db2aa100a618df5800153a
shinjunseok <sjsage522@gmail.com>
2021-09-30 17:26:38 +0900

    update : 인증에러시 응답 포맷팅

--- src/main/java/io/wisoft/vamos/security/RestAuthenticationEntryPoint.java
+++ src/main/java/io/wisoft/vamos/security/RestAuthenticationEntryPoint.java
@@ -1,14 +1,16 @@
 package io.wisoft.vamos.security;
 
+import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
@@ -17,9 +19,19 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest,
                          HttpServletResponse httpServletResponse,
-                         AuthenticationException e) throws IOException, ServletException {
+                         AuthenticationException e) throws IOException {
         logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                e.getLocalizedMessage());
+
+        httpServletResponse.setStatus(SC_UNAUTHORIZED);
+        JSONObject json = new JSONObject();
+        json.put("data", null);
+
+        JSONObject errorJson = new JSONObject();
+        errorJson.put("message", e.getLocalizedMessage());
+        errorJson.put("status", SC_UNAUTHORIZED);
+        json.put("error", errorJson);
+
+        httpServletResponse.getOutputStream()
+                .println(json.toJSONString());
     }
 }
