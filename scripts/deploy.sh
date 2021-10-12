9b963d2b9f5334c44fbade25985b493ebcd70148
shinjunseok <sjsage522@gmail.com>
2021-10-12 22:52:14 +0900

    fix : 오탈자 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,5 +28,5 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,classpath:/application-prod.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-property.yml \
+    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-prod.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-property.yml \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
