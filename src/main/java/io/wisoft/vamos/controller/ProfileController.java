7e7984077a5a58bb5a6a43332f6c4bb56a4c76a0
shinjunseok <sjsage522@gmail.com>
2021-10-13 15:02:59 +0900

    update : log 추가

--- src/main/java/io/wisoft/vamos/controller/ProfileController.java
+++ src/main/java/io/wisoft/vamos/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package io.wisoft.vamos.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 @RestController
 public class ProfileController {
 
@@ -20,6 +22,7 @@ public class ProfileController {
         List<String> productionProfiles = Arrays.asList("prod1", "prod2");
         String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
 
+        log.info("profiles : {}", profiles);
         return profiles.stream()
                 .filter(productionProfiles::contains)
                 .findAny()
