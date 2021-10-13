22aa8399751a7b9f8d756301a18d2dc0bb41431d
shinjunseok <sjsage522@gmail.com>
2021-10-13 15:02:48 +0900

    fix : 오탈자 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,10 +15,10 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'prod' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+  then # $up_count >= 1 ("prod" 문자열이 있는지 검증)
       echo "> Health check 성공"
       switch_proxy
       break
