ca55eab210c3b275a0a1eb269cab98341d50ea06
shinjunseok <sjsage522@gmail.com>
2021-10-19 15:30:17 +0900

    update : ip check 로직 변경

--- src/main/java/io/wisoft/vamos/security/IpAddressCheckFilter.java
+++ src/main/java/io/wisoft/vamos/security/IpAddressCheckFilter.java
@@ -9,14 +9,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Component
 public class IpAddressCheckFilter extends OncePerRequestFilter {
 
+    private final List<String> keys = List.of(
+            "X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
+            "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR");
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        log.info("client ip = {}", request.getRemoteAddr());
+
+        boolean isCheck = false;
+        for (String key : keys) {
+            String clientIp = request.getHeader(key);
+            if (clientIp != null) {
+                log.info("client ip = {}", clientIp);
+                isCheck = true;
+                break;
+            }
+        }
+
+        if (!isCheck) log.info("client ip = {}", request.getRemoteAddr());
+
         filterChain.doFilter(request, response);
     }
 }
