8383fe264f79afbd57121fda4a7fe5e75c09ebcc
shinjunseok <sjsage522@gmail.com>
2021-10-19 11:13:56 +0900

    add : filter 추가

---
+++ src/main/java/io/wisoft/vamos/security/IpAddressCheckFilter.java
@@ -0,0 +1,22 @@
+package io.wisoft.vamos.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class IpAddressCheckFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("client ip = {}", request.getRemoteAddr());
+        filterChain.doFilter(request, response);
+    }
+}
