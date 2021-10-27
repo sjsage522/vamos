c4ee0d4ca756174c1e5b3a4cad016314af3a477b
shinjunseok <sjsage522@gmail.com>
2021-10-27 20:06:36 +0900

    fix : 카테고리 0번부터 시작으로 수정

--- src/main/java/io/wisoft/vamos/domain/category/CategoryName.java
+++ src/main/java/io/wisoft/vamos/domain/category/CategoryName.java
@@ -5,22 +5,22 @@ import lombok.Getter;
 @Getter
 public enum CategoryName {
 
-    DIGITAL_DEVICE(1L, "DIGITAL_DEVICE", "디지털기기"),
-    HOME_APPLIANCES(2L, "HOME_APPLIANCES", "생활가전"),
-    FURNITURE_INTERIOR(3L, "FURNITURE_INTERIOR", "가구/인테리어"),
-    INFANT_CHILD(4L, "INFANT_CHILD", "유아동"),
-    TODDLER_BOOK(5L, "TODDLER_BOOK", "유아도서"),
-    PROCESSED_FOOD(6L, "PROCESSED_FOOD", "생활/가공식품"),
-    SPORTS_LEISURE(7L, "SPORTS_LEISURE", "스포츠/레저"),
-    WOMEN_FASHION(8L, "WOMEN_FASHION", "여성패션/잡화"),
-    MEN_FASHION(9L, "MEN_FASHION", "남성패션/잡화"),
-    GAME_HOBBY(10L, "GAME_HOBBY", "게임/취미"),
-    BEAUTY(11L, "BEAUTY", "뷰티/미용"),
-    PET_SUPPLIES(12L, "PET_SUPPLIES", "반려동물용품"),
-    BOOKS_TICKETS_RECORDS(13L, "BOOKS_TICKETS_RECORDS", "도서/티켓/음반"),
-    PLANT(14L, "PLANT", "식물"),
-    ETC(15L, "ETC", "기타 중고물품"),
-    WANT_BUY(16L, "WANT_BUY", "삽니다")
+    DIGITAL_DEVICE(0L, "DIGITAL_DEVICE", "디지털기기"),
+    HOME_APPLIANCES(1L, "HOME_APPLIANCES", "생활가전"),
+    FURNITURE_INTERIOR(2L, "FURNITURE_INTERIOR", "가구/인테리어"),
+    INFANT_CHILD(3L, "INFANT_CHILD", "유아동"),
+    TODDLER_BOOK(4L, "TODDLER_BOOK", "유아도서"),
+    PROCESSED_FOOD(5L, "PROCESSED_FOOD", "생활/가공식품"),
+    SPORTS_LEISURE(6L, "SPORTS_LEISURE", "스포츠/레저"),
+    WOMEN_FASHION(7L, "WOMEN_FASHION", "여성패션/잡화"),
+    MEN_FASHION(8L, "MEN_FASHION", "남성패션/잡화"),
+    GAME_HOBBY(9L, "GAME_HOBBY", "게임/취미"),
+    BEAUTY(10L, "BEAUTY", "뷰티/미용"),
+    PET_SUPPLIES(11L, "PET_SUPPLIES", "반려동물용품"),
+    BOOKS_TICKETS_RECORDS(12L, "BOOKS_TICKETS_RECORDS", "도서/티켓/음반"),
+    PLANT(13L, "PLANT", "식물"),
+    ETC(14L, "ETC", "기타 중고물품"),
+    WANT_BUY(15L, "WANT_BUY", "삽니다")
     ;
 
     private Long no;
