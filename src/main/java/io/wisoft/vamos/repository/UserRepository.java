97c39d70fef8d903a9104ad14c5103ac9c1959c9
shinjunseok <sjsage522@gmail.com>
2021-11-05 14:58:11 +0900

    update : 닉네임 존재 여부 쿼리 추가

--- src/main/java/io/wisoft/vamos/repository/UserRepository.java
+++ src/main/java/io/wisoft/vamos/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Boolean existsByEmail(String email);
+
+    Boolean existsByNickname(String nickname);
 }
