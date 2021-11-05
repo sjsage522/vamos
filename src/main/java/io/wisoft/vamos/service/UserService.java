737a84766ebd1990f06b741abacac38774ed6917
shinjunseok <sjsage522@gmail.com>
2021-11-05 16:44:00 +0900

    update : 사용자 정보 수정 로직 추가

--- src/main/java/io/wisoft/vamos/service/UserService.java
+++ src/main/java/io/wisoft/vamos/service/UserService.java
@@ -3,9 +3,11 @@ package io.wisoft.vamos.service;
 import io.wisoft.vamos.domain.user.User;
 import io.wisoft.vamos.domain.user.UserLocation;
 import io.wisoft.vamos.dto.user.UserLocationUpdateRequest;
+import io.wisoft.vamos.dto.user.UserUpdateRequest;
 import io.wisoft.vamos.exception.NoMatchUserInfoException;
 import io.wisoft.vamos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +18,7 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
-
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional(readOnly = true)
     public User findByEmail(String email) {
@@ -50,6 +52,20 @@ public class UserService {
         User findUser = findByEmail(email);
         UserLocation location = UserLocation.from(request.getX(), request.getY(), request.getAddressName());
         findUser.changeUserLocation(location);
+        return findUser;
+    }
+
+    @Transactional
+    public User updateUser(String email, UserUpdateRequest request) {
+        User findUser = findByEmail(email);
+
+        User updateUser = User.builder()
+                .nickname(request.getNickname())
+                .password(request.getPassword() != null ? passwordEncoder.encode(request.getPassword()) : null)
+                .picture(request.getPicture())
+                .build();
+        findUser.update(updateUser);
+
         return findUser;
     }
 }
