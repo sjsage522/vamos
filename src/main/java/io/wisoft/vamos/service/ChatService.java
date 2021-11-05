d7aff978e1c56e4ab28f5def10b565344d9a8d86
shinjunseok <sjsage522@gmail.com>
2021-11-05 13:49:18 +0900

    update : 채팅방 삭제 로직 추가

--- src/main/java/io/wisoft/vamos/service/ChatService.java
+++ src/main/java/io/wisoft/vamos/service/ChatService.java
@@ -6,6 +6,7 @@ import io.wisoft.vamos.domain.user.User;
 import io.wisoft.vamos.dto.chat.ChatContentRequest;
 import io.wisoft.vamos.exception.NoMatchChattingRoomException;
 import io.wisoft.vamos.exception.NoMatchUserInfoException;
+import io.wisoft.vamos.repository.BoardRepository;
 import io.wisoft.vamos.repository.ChatContentRepository;
 import io.wisoft.vamos.repository.ChatRoomRepository;
 import io.wisoft.vamos.repository.UserRepository;
@@ -13,8 +14,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -37,6 +40,22 @@ public class ChatService {
     @Transactional
     public ChattingRoom createChatRoom(ChattingRoom initChatRoom) {
         return chatRoomRepository.save(initChatRoom);
+    }
+
+    @Transactional
+    public void deleteChatRoomsByBoardId(Long boardId) {
+        List<ChattingRoom> chattingRooms = chatRoomRepository.findChattingRoomWithContentsByBoardId(boardId);
+
+        List<Long> roomIds = new ArrayList<>();
+        for (ChattingRoom chattingRoom : chattingRooms) {
+            roomIds.add(chattingRoom.getId());
+            List<Long> contentIds = chattingRoom.getChattingContents().stream()
+                    .map(ChattingContent::getId)
+                    .collect(Collectors.toList());
+            chatContentRepository.deleteWithIds(contentIds);
+        }
+
+        chatRoomRepository.deleteWithIds(roomIds);
     }
 
     @Transactional(readOnly = true)
