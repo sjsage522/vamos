b314d1d9f8756c0413d664dbb5d9c490921ff96a
shinjunseok <sjsage522@gmail.com>
2021-11-05 13:50:17 +0900

    update : in 절로 삭제 쿼리 추가

--- src/main/java/io/wisoft/vamos/repository/ChatContentRepository.java
+++ src/main/java/io/wisoft/vamos/repository/ChatContentRepository.java
@@ -2,10 +2,16 @@ package io.wisoft.vamos.repository;
 
 import io.wisoft.vamos.domain.chatting.ChattingContent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ChatContentRepository extends JpaRepository<ChattingContent, Long> {
 
     List<ChattingContent> findAllByChattingRoomId(Long chattingRoomId);
+
+    @Modifying
+    @Query("delete from ChattingContent c where c.id in :ids")
+    void deleteWithIds(List<Long> ids);
 }
