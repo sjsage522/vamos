d294e916e20e1cbf20ca95d9e2198542b15c5ec0
shinjunseok <sjsage522@gmail.com>
2021-11-21 15:48:35 +0900

    update : 스크립트 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,13 +5,13 @@
 # 쉬고 있는 profile 찾기: prod1이 사용중이면 prod2가 쉬고 있고, 반대면 prod1이 쉬고 있음
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://api.vamos.pw/profile)
 
     if [ "${RESPONSE_CODE}" -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=prod2
     else
-        CURRENT_PROFILE=$(curl -s -k https://localhost/profile)
+        CURRENT_PROFILE=$(curl -s -k https://api.vamos.pw/profile)
     fi
 
     if [ "${CURRENT_PROFILE}" == prod1 ]
